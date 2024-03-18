f7bdcbb8d07b532f49db2734ae60f966f1338b3d
nnamss <10407499+nnamss@users.noreply.github.com>
2024-03-17 21:39:20 -0400

    publish dist

---
+++ dist/types/package.d.ts
@@ -0,0 +1,10 @@
+declare namespace _default {
+    const name: string;
+    const version: string;
+    const license: string;
+    const author: string;
+    const description: string;
+    const repository: string;
+    const files: string[];
+}
+export default _default;
